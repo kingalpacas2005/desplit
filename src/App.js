f2f561fe3589c3c0fdbb7536eded615f902479eb
Alexis <73175623+Sailxe@users.noreply.github.com>
2022-04-09 20:52:46 +0200

    transaction

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MakeGroup from "./pages/MakeGroup";
 import Home from "./pages/Home";
 import GroupTransactions from "./pages/GroupTransactions";
+import Transaction from "./pages/Transaction";
 import NotFound from "./pages/NotFound";
 
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/groups/create" exact component={MakeGroup} />
         <Route path="/groups/:id/transactions" exact component={GroupTransactions} />
+        <Route path="/transaction/create" exact component={Transaction} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
