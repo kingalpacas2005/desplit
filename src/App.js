21b754a7c88810dea9114d1d97a79bdfcdf5170a
Martial GARCHERY <m.garchery@unyc.io>
2022-04-09 22:24:58 +0200

    group create

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import MakeGroup from "./pages/MakeGroup";
+import GroupCreate from "./pages/GroupCreate";
 import Home from "./pages/Home";
 import GroupTransactions from "./pages/GroupTransactions";
 import Transaction from "./pages/Transaction";
@@ -9,15 +9,15 @@ import NotFound from "./pages/NotFound";
 
 function App() {
 
-    return ( < BrowserRouter >
-        <Switch >
-        <Route path = "/" exact component = { Home }/> 
-        <Route path = "/groups/create" exact component = { MakeGroup }/> 
-        <Route path = "/groups/:id/transactions"exact component = { GroupTransactions }/> 
-        <Route path = "/transaction/create" exact component = { Transaction }/> 
-        <Route component = { NotFound }/>
-        </Switch > </BrowserRouter>
-    );
+  return (< BrowserRouter >
+    <Switch >
+      <Route path="/" exact component={Home} />
+      <Route path="/groups/create" exact component={GroupCreate} />
+      <Route path="/groups/:id/transactions" exact component={GroupTransactions} />
+      <Route path="/transaction/create" exact component={Transaction} />
+      <Route component={NotFound} />
+    </Switch > </BrowserRouter>
+  );
 }
 
 export default App;
