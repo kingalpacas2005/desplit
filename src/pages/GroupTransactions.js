0d126b4b1a75603ea674fb9ba993f93c97c92948
kingalpacas2005 <canalcuriosidadesdaterra@gmail.com>
2022-04-10 13:23:00 +0100

    Change group

--- src/pages/GroupTransactions.js
+++ src/pages/GroupTransactions.js
@@ -17,35 +17,11 @@ const GroupTransactions = () => {
 
         </div>
 
-        <div className = "addressesgroup" >
-        <div className = "a1" >
-        <div className = "left" >
-        <p className = "address" > 0xd35781d9712436ad6dcad30a51e408d4232781ef7296d9a8d53f4c7203744809 </p> <div className = "slash" > - </div> 
-        <p className = "value" > 1.000, 29 $ </p> </div>
-
-        <div className = "right" >
-        <p className = "accept" > Accept </p> <p className = "slash" >
-        </p> <p className = "reject" > Reject </p> </div> </div> 
-        <div className = "a1" >
-        <div className = "left" >
-        <p className = "address" > 0xd35781d9712436ad6dcad30a51e408d4232781ef7296d9a8d53f4c7203744809 </p>
-        <div className = "slash" > - </div> <p className = "value" > 1.000, 29 $ </p> </div>
-
-        <div className = "right" >
-        <p className = "accept" > Accept </p> <p className = "slash" > | </p> <p className = "reject" > Reject </p> </div> </div> <div className = "a1" >
-        <div className = "left" >
-        <p className = "address" > 0xd35781d9712436ad6dcad30a51e408d4232781ef7296d9a8d53f4c7203744809 </p> 
-        <div className = "slash" > - </div> <p className = "value" > 1.000, 29 $ </p> </div>
-
-        <div className = "right" >
-        <p className = "accept" > Accept </p> <p className = "slash" > | </p> <p className = "reject" > Reject </p> </div> </div>
 
 
-
-
-
-
-        </div> </div> </body> </div>
+        </div>
+        </body>
+    </div >
     );
 };
 export default GroupTransactions;
