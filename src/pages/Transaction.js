09b31e341c04d803ceac56d90557902a67206b74
Martial GARCHERY <m.garchery@unyc.io>
2022-04-10 14:43:15 +0200

    fix

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -4,7 +4,7 @@ import ChooseMoney from '../components/ChooseMoney';
 const Transaction = () => {
     return (
         <div className="transactionPage">
-            <div className="PositionMetaMask"><MetaMask></MetaMask></div>
+            <div className="PositionMetaMask"></div>
             <h2 className="header">DeSplit</h2>
             <div className="borderFormulaire">
                 <form className="formulaire ">
@@ -26,11 +26,11 @@ const Transaction = () => {
                             <input type="text" name="Amount" className="box"></input>
                         </div>
                     </div>
-                    
-                    
+
+
                     <div className="elementForm elementFormCheck checkAndMoney"><input type="checkbox" name="checkTransaction"></input><label for="checkTransaction">Immediate settlement off   </label>  <div className="espace2"></div>   <ChooseMoney></ChooseMoney></div>
                     <div className="elementForm"><input type="submit" value="Submit" className="Submitform"></input></div>
-                    
+
                 </form>
             </div>
         </div>
