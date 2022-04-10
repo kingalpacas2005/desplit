9791763bd36542805528a967ef9abb8cf5f1474a
Martial GARCHERY <m.garchery@unyc.io>
2022-04-10 10:48:48 +0200

    wallet connection working

--- src/components/MetaMask.js
+++ src/components/MetaMask.js
@@ -1,11 +1,56 @@
 import React from 'react';
 
-const MetaMask = () => {
-    return (
-        <div className="metaMask">
-            <img src="./../img/MetaMask.jpg" className="MetaMaskImage" /><p>METAMASK</p>
-        </div>
-    );
+
+class MetaMask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            accountAddress: "",
+        };
+    }
+
+    async connectWallet() {
+        if (window.ethereum) {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            this.setState({ accountAddress: accounts[0] });
+            console.log(this.state.accountAddress);
+            alert("Connected !");
+        }
+    }
+
+    renderAddress() {
+        if (this.state.accountAddress) {
+            return (
+                <div>
+                    <img src="./../img/MetaMask.jpg" className="MetaMaskImage" />
+                    <p>
+                        Address: {this.state.accountAddress}
+                    </p>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <p>
+                        Address: {this.state.accountAddress}
+                    </p>
+                    <img src="./../img/MetaMask.jpg" className="MetaMaskImage" />
+                    <p>METAMASK</p>
+                    <button onClick={() => this.connectWallet()}>Connect Wallet</button>
+                </div>
+            )
+
+        }
+    }
+    render() {
+        return (
+            <div className="metaMask" >
+                {this.renderAddress()}
+            </div>
+        )
+    }
 };
+
 
 export default MetaMask;
